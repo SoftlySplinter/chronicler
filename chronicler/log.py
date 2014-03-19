0cce1736f81fbf92e2e781005e82019b601a9d22
Alex Brown <alex@alexanderdbrown.com>
2014-03-19 08:11:43 +0000

    Improve log formatting further.

--- chronicler/log.py
+++ chronicler/log.py
@@ -1,7 +1,7 @@
 import json
 import logging
 
-from pyparsing import Word, alphas, Suppress, Combine, nums, string, Optional, Regex, ParseException, Empty
+from pyparsing import Word, alphas, Suppress, Combine, nums, string, Optional, Regex, ParseException, Empty, Group
 from time import strftime
 
 ints = Word(nums)
@@ -15,7 +15,7 @@ class LogFormat(object):
     'msg': Regex(".*"),
     'rawmsg': Regex(".*"),
     'HOSTNAME': Word(alphas + nums + "_" + "-" + "."),
-    'syslogtag': Word(alphas + "/" + "-" + "_" + ".") + Optional(Suppress("[") + ints + Suppress("]")) + Suppress(":"),
+    'syslogtag': Group(Word(alphas + "/" + "-" + "_" + ".") + Optional(Suppress("[") + ints + Suppress("]")) + Suppress(":")),
     'programname': Word(alphas + "/" + "-" + "_" + ".") + Optional(Suppress("[") + Suppress(ints) + Suppress("]") + Suppress(":")),
     'PRI': ints,
     'syslogfacility': None,
@@ -24,15 +24,16 @@ class LogFormat(object):
     'syslogseverity-text': None,
     'timegenerated': None,
     'timereported': None,
-    'TIMESTAMP': month + day + hour
+    'TIMESTAMP': Group(month + day + hour)
   }
 
   def __init__(self, fmt):
     self.format = fmt
-    self.pattern = self.gen_format(fmt)
+    self.pattern, self.properties = self.gen_format(fmt)
 
   def gen_format(self, fmt):
     res = Empty()
+    props = []
     escape = False
     prop = ""
     for char in fmt:
@@ -40,6 +41,7 @@ class LogFormat(object):
         if char == "%":
           escape = False
           res = res + self.__properties[prop]
+          props.append(prop)
           prop = ""
         else:
           prop = prop + char
@@ -50,16 +52,20 @@ class LogFormat(object):
           pass
         else:
           res = res + Suppress(char)
-    return res
+    return res, props
 
   def parse(self, line, callback):
     try:
       res = {}
       parsed = self.pattern.parseString(line)
-      res['TIMESTAMP'] = strftime("%Y-%m-%d %H:%M:%S")
-      res['HOSTNAME'] = parsed[3]
-      res['syslogtag'] = parsed[4:-1]
-      res['msg'] = parsed[-1]
+      i = 0
+      for prop in self.properties:
+        if prop == "TIMESTAMP":
+          res[prop] = strftime("%Y-%m-%d %H:%M:%S")
+          i += 1
+        else:
+          res[prop] = parsed[i]
+          i += 1
       callback(res)
     except ParseException as e:
       logging.warn("Unable to parse line: {0}".format(line))
--- test_script.sh
+++ test_script.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 curl http://127.0.0.1:5000/start -X POST
-curl http://127.0.0.1:5000/system/logs -X POST --data @syslog.json -H "Content-Type:application/json"
-curl http://127.0.0.1:5000/system/logs -X GET -H "Accept:application/json"
-curl http://127.0.0.1:5000/system/logs -X POST --data @syslog.json -H "Content-Type:application/json"
+curl http://127.0.0.1:5000/system/logs -X POST --data @examples/syslog.json -H "Content-Type:application/json"
 curl http://127.0.0.1:5000/system/logs -X GET -H "Accept:application/json"
 curl http://127.0.0.1:5000/system/logs/-6477767910910693092 -X GET -H "Accept:application/json"
+sleep 10
+curl http://127.0.0.1:5000/system/logs -X POST --data @examples/sample.json -H "Content-Type:application/json"
