f635004f9a2e86d1803bd551c2abe871af4d2c6d
Alex Brown <alex@alexanderdbrown.com>
2014-07-03 18:22:32 +0100

    Moved API to a separate directory.

--- chronicler.js
+++ chronicler.js
@@ -1,7 +1,6 @@
 restify = require('restify');
-tv4 = require('tv4');
-daemonSchema = require(__dirname + '/definitions/daemon');
-daemon = require('./src/daemon.js');
+
+daemonApi = require('./src/api/daemon.js');
 
 server = restify.createServer({
   "name": "Chronicler"
@@ -9,56 +8,6 @@ server = restify.createServer({
 
 server.use(restify.bodyParser());
 
-server.get("/", function(req, res, next) {
-  res.json({
-    "Chronicler": "0.0.0"
-  });
-  return next();
-});
-
-server.post("/daemon", function(req, res, next) {
-  if(!req.is('json')) {
-    return next(new restify.InvalidContentError("Expected JSON"));
-  }
-
-  if(!tv4.validate(req.body, daemonSchema)) {
-    return next(new restify.InvalidContentError("Invalid daemon definition"));
-  }
-
-  try {
-    res.json(201, daemon.create(req.body));
-    return next();
-  } catch (ex) {
-    return next(new restify.InvalidContentError(ex.message));
-  }
-});
-
-server.get("/daemon/:name", function(req, res, next) {
-  try {
-    res.json(daemon.get(req.params.name));
-    return next();
-  } catch (ex) {
-    return next(new restify.InvalidContentError(ex.message));
-  }
-});
-
-server.patch("/daemon/:name", function(req, res, next) {
-  try {
-    res.json(daemon.pause(req.params.name, req.body.pause));
-    return next();
-  } catch (ex) {
-    return next(new restify.InvalidContentError(ex.message));
-  }
-})
-
-server.del("/daemon/:name", function(req, res, next) {
-  try {
-    daemon.del(req.params.name);
-    res.send(200);
-    return next();
-  } catch (ex) {
-    return next(new restify.InvalidContentError(ex.message));
-  }
-})
+daemonApi.register(server);
 
 server.listen(8080);
---
+++ src/api/daemon.js
@@ -0,0 +1,61 @@
+tv4 = require('tv4');
+daemonSchema = require('../../definitions/daemon');
+daemon = require('../daemon.js');
+
+function DaemonApi() {}
+
+DaemonApi.prototype.register = function(server) {
+  server.get("/", function(req, res, next) {
+    res.json({
+      "Chronicler": "0.0.0"
+    });
+    return next();
+  });
+  
+  server.post("/daemon", function(req, res, next) {
+    if(!req.is('json')) {
+      return next(new restify.InvalidContentError("Expected JSON"));
+    }
+  
+    if(!tv4.validate(req.body, daemonSchema)) {
+      return next(new restify.InvalidContentError("Invalid daemon definition"));
+    }
+  
+    try {
+      res.json(201, daemon.create(req.body));
+      return next();
+    } catch (ex) {
+      return next(new restify.InvalidContentError(ex.message));
+    }
+  });
+  
+  server.get("/daemon/:name", function(req, res, next) {
+    try {
+      res.json(daemon.get(req.params.name));
+      return next();
+    } catch (ex) {
+      return next(new restify.InvalidContentError(ex.message));
+    }
+  });
+  
+  server.patch("/daemon/:name", function(req, res, next) {
+    try {
+      res.json(daemon.pause(req.params.name, req.body.pause));
+      return next();
+    } catch (ex) {
+      return next(new restify.InvalidContentError(ex.message));
+    }
+  });
+  
+  server.del("/daemon/:name", function(req, res, next) {
+    try {
+      daemon.del(req.params.name);
+      res.send(200);
+      return next();
+    } catch (ex) {
+      return next(new restify.InvalidContentError(ex.message));
+    }
+  });
+}
+
+module.exports = new DaemonApi();
